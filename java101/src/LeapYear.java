2e2cdee72d2ce4ca6daba1fac742479ce7bb7875
zachriel <topraktas19@hotmail.com>
2022-10-16 22:38:28 +0300

    added

---
+++ java101/src/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        int year;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the year: ");
+        year = sc.nextInt();
+
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0) System.out.println(year + " is a leap year.");
+                else {
+                    System.out.println(year + " is not a leap year.");
+                }
+            }else System.out.println(year + " is a leap year.");
+        }else System.out.println(year + " is not a leap year.");
+    }
+}
