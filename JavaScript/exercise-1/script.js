02e33e3b1fbc9c14f06abc7221057dda1a7385a9
Toprak Aktaş <topraktas19@hotmail.com>
2023-02-26 03:52:23 +0200

    initial commit

---
+++ JavaScript/exercise-1/script.js
@@ -0,0 +1,13 @@
+let counter = 0
+let counterDOM = document.getElementById('counter')
+let increaseDOM = document.getElementById('increase')
+let decreaseDOM = document.getElementById('decrease')
+
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+function clickEvent(){
+    console.log(this.id)
+    this.id == "increase" ? counter += 1 : counter -= 1
+    counterDOM.innerHTML = counter
+}
